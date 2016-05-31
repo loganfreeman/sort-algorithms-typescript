96297fc831ca08e6ef2a7af8c75621a9e7e7b126
shanhong cheng <shanhong.cheng@utah.edu>
2016-05-31 14:20:13 -0600

    format

--- test/sort.ts
+++ test/sort.ts
@@ -18,46 +18,46 @@ const tests = {
     heap
 };
 
-Object.keys(tests).forEach((item: string)=> {
-    describe(`Sort ${item}`, ()=> {
+Object.keys(tests).forEach((item: string) => {
+    describe(`Sort ${item}`, () => {
         const sort: Function = tests[item];
 
-        it('Should sort long array', ()=> {
+        it('Should sort long array', () => {
             let arr: Array<number> = [1, 5, 9, 12, -10, 0, 15, 20, 100, 4];
             let arrCorrect: Array<number> = [-10, 0, 1, 4, 5, 9, 12, 15, 20, 100];
 
             expect(sort(arr)).to.eql(arrCorrect);
         });
 
-        it('Should sort empty array', ()=> {
+        it('Should sort empty array', () => {
             expect(sort([])).to.eql([]);
         });
 
-        it('Should sort one element array', ()=> {
+        it('Should sort one element array', () => {
             expect(sort([1])).to.eql([1]);
         });
 
-        it('Should sort [2, 1] array', ()=> {
+        it('Should sort [2, 1] array', () => {
             expect(sort([2, 1])).to.eql([1, 2]);
         });
 
-        it('Should sort [1, 0, 0, 0]', ()=> {
+        it('Should sort [1, 0, 0, 0]', () => {
             expect(sort([1, 0, 0, 0])).to.eql([0, 0, 0, 1]);
         });
 
-        it('Should sort [1, 0, 0, 1]', ()=> {
+        it('Should sort [1, 0, 0, 1]', () => {
             expect(sort([1, 0, 0, 1])).to.eql([0, 0, 1, 1]);
         });
 
-        it('Should sort [0, 0, 0, 1]', ()=> {
+        it('Should sort [0, 0, 0, 1]', () => {
             expect(sort([0, 0, 0, 1])).to.eql([0, 0, 0, 1]);
         });
 
-        it('Should sort [0, 0, 0, 0]', ()=> {
+        it('Should sort [0, 0, 0, 0]', () => {
             expect(sort([0, 0, 0, 0])).to.eql([0, 0, 0, 0]);
         });
 
-        it('Should sort [0, 3, 0]', ()=> {
+        it('Should sort [0, 3, 0]', () => {
             expect(sort([0, 3, 0])).to.eql([0, 0, 3]);
         });
     });
--- test/toposort.ts
+++ test/toposort.ts
@@ -2,16 +2,16 @@ import {toposort} from '../src/toposort';
 import {expect} from 'chai';
 
 describe('toposort', () => {
-  let graph = [
-  ['put on your shoes', 'tie your shoes']
-, ['put on your shirt', 'put on your jacket']
-, ['put on your shorts', 'put on your jacket']
-, ['put on your shorts', 'put on your shoes']
-];
+    let graph = [
+        ['put on your shoes', 'tie your shoes']
+        , ['put on your shirt', 'put on your jacket']
+        , ['put on your shorts', 'put on your jacket']
+        , ['put on your shorts', 'put on your shoes']
+    ];
 
-  it('should sort topologically', () => {
-    let sorted = toposort(graph);
-    expect(sorted).to.eql(['put on your shirt', 'put on your shorts', 'put on your jacket', 'put on your shoes', 'tie your shoes']);
-  })
+    it('should sort topologically', () => {
+        let sorted = toposort(graph);
+        expect(sorted).to.eql(['put on your shirt', 'put on your shorts', 'put on your jacket', 'put on your shoes', 'tie your shoes']);
+    })
 
 })
